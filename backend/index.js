a0412edbf1f8780f7c20eb4ee4a4072a95c7fcc0
Priyanka Thakur <priyankathakur221206@gmail.com>
2022-02-23 01:07:37 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const cors = require("cors");
+
+const { Server } = require("socket.io");
+
+app.use(cors());
+
