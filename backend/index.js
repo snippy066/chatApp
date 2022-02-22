ff3821ad0cd8e040bc0dd27759ba7ec8289c1516
snippy066 <priyankathakur221206@gmail.com>
2022-02-23 01:11:06 +0530

    set io cors problems

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,11 @@ const { Server } = require("socket.io");
 
 app.use(cors());
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
