1c7c0e80c843d0e334ba17808e7db45b42f2730f
snippy066 <priyankathakur221206@gmail.com>
2022-02-23 01:44:47 +0530

    base setup of frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,23 @@ const io = new Server(server, {
   },
 });
 
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
+  
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+  
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id);
+    });
+  });
+
 server.listen(3001, () => {
     console.log("SERVER RUNNING");
   });
